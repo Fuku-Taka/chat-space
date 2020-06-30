4c869b7fef38f1490d48c8d0985fe51647817219
Fuku-Taka <67095267+Fuku-Taka@users.noreply.github.com>
2020-06-30 11:00:33 +0900

    update-MessageSend

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :edit, :update, :new, :create]
+  resources :groups, only: [:edit, :update, :new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
